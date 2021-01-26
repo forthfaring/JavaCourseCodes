509ea06aed81f1d318b77179e93bb43cd9363f47
wfy <wfy@ebidding.net.cn>
2021-01-26 23:55:35 +0800

    pool test

---
+++ 03concurrency/0301/src/main/java/java0/conc0301/pool/ThreadPoolTest.java
@@ -0,0 +1,19 @@
+package java0.conc0301.pool;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: 0301
+ * @description:
+ * @author: wfy
+ * @create: 2021-01-26 23:33
+ */
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        TimeUnit unit;
+        BlockingQueue workQueue;
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, , unit, workQueue);
+    }
+}
